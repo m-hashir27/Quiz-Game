f1ab00e06d123d85c684d7921ce3902adf63b17f
Hashir <hashirbinatif83@gmail.com>
2025-07-11 17:37:18 +0500

    First commit of this project

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb:1, 
+        question: "What does HTML stand for?",
+        answer:"A. Hyper Text Markup Language",
+        options:[
+            "A. Hyper Text Markup Language",
+            "B. Hyperlinks and Text Markup Language",
+            "C. High Text Markup Language",
+            "D. None of the above"
+        ]
+    },
+     {
+        numb:2, 
+        question: "What does CSS stand for?",
+        answer:"C. Cascading Style Sheets",
+        options:[
+            "A. Cascade Style Sheets",
+            "B. Cascading Style Sheet",
+            "C. Cascading Style Sheets",
+            "D. None of the above"
+        ]
+    },
+     {
+        numb:3, 
+        question: "What does JS stand for?",
+        answer:"C. JavaScript",
+        options:[
+            "A. JavacScript",
+            "B. JavaScripts",
+            "C. JavaScript",
+            "D. None of the above"
+        ]
+    },
+     {
+        numb:4, 
+        question: "What does DOM stand for?",
+        answer:"B. Document Object Model",
+        options:[
+            "A. Document Orientation Model",
+            "B. Document Object Model ",
+            "C. Document Object Module",
+            "D. None of the above"
+        ]
+    },
+     {
+        numb:5, 
+        question: "What does API stand for?",
+        answer:"C. Application Programming Interface",
+        options:[
+            "A. Application Programming Interfacing",
+            "B. Application Program Interface",
+            "C. Application Programming Interface ",
+            "D. None of the above"
+        ]
+    }
+]
